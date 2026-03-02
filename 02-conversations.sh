5af776ca8807558ef894002735a6a631e2ace5b3
AnilKumar <anilattada222@gmail.com>
2026-03-02 21:29:16 +0530

    helloword modified

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -3,6 +3,7 @@
 Person1=Modi
 Person2=Musk
 
-Person1:: Hi Musk,are you?
-Person2:: Hi, Iam good, What about you?
+echo "$Person1:: Hi Musk,are you?"
+echo "$Person2:: Hi, Iam good, What about you?"
+
 
