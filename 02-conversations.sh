8be3879670cff5ca73d456b387159b2609f03e31
AnilKumar <anilattada222@gmail.com>
2026-03-03 20:09:29 +0530

    conversation added

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+echo "Modi ::Hi Muskk, How are you?"
+echo "Musk ::Hi, Iam fine, What about you?"
+echo "Modi ::Iam good."
 
-Modi ::Hi Muskk, How are you?
-Musk ::Hi, Iam fine, What about you?
-Modi ::Iam good.
 
