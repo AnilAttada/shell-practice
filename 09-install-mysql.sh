e7c82b2b1da9654ff5c0ec6577f7626a2285fc94
AnilKumar <anilattada222@gmail.com>
2026-03-03 20:52:06 +0530

    INSTALL-MYSQL added

---
+++ 09-install-mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run script with root access"
+    exit 1
+else
+    echo "you are running as root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installing Mysql...SUCCESS"
+else 
+    echo "Installing Mysql...FAILURE"
+    exit 1
+fi
