eafe28b657b169b6c63ad2e6f04ddbd574d039b0
AnilKumar <anilattada222@gmail.com>
2026-03-04 21:38:17 +0530

    FUNCTIONS added

--- 10-functions.sh
+++ 10-functions.sh
@@ -9,20 +9,22 @@ else
     echo "running with root access"
 fi
 
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo "$2 is installed....SUCCESS"
+else   
+    echo " $2 is not installed.....FAILURE"
+fi
+
+}
 dnf list installed httpd
 
 if [ $? -ne 0 ]
 then
     echo "httpd is not installed......going to install"
 dnf install httpd -y
-
-if [ $? -eq 0 ]
-then
-    echo "httpd is installed....SUCCESS"
-else   
-    echo " httpd is not installed.....FAILURE"
-fi
-
+VALIDATE $? "httpd
 else
     echo " httpd is installed...already present"
 fi
