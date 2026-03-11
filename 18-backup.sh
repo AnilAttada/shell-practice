083d2bc23e0fc02d0fda8571c9066177de26d796
AnilKumar <anilattada222@gmail.com>
2026-03-11 22:08:10 +0530

    ROBOSHOP added

---
+++ 18-backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DATE=${3:-14}
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e "$G running with root access $N" | tee -a $LOG_FILE
+fi
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 18-backup.sh <source-dir> <dest-dir> <days(optional)>"
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is installed....$G SUCCESS $N" | tee -a $LOG_FILE
+    else    
+        echo -e "$2 is not installed..... $R FAILURE $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
