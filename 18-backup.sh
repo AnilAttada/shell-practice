acbad1b85e35d76938a44087abef30fda49f3534
AnilKumar <anilattada222@gmail.com>
2026-03-13 19:27:32 +0530

    ROBOSHOP added

--- 18-backup.sh
+++ 18-backup.sh
@@ -11,8 +11,8 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #This is for main script
-#LOG_FILE="$LOGS_FOLDER/backup.log"
+#LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #This is for main script
+LOG_FILE="$LOGS_FOLDER/backup.log"
 
 mkdir -p $LOGS_FOLDER
 
